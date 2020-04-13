68ed31aeccb0a7ad671fa445ccef50b9b97ff9f1
manekmehra <61108498+manekmehra@users.noreply.github.com>
2020-04-13 17:24:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+var r = 0;
+var g = 50;
+var b=255;
+ 
+
+
+function setup(){
+createCanvas(1200,400);
+var r = create rect(400,400,400,400)
+var g = create rect(600,400,400,400)
+var b = create rect(800,400,400,400)
+}
+
+
+function draw() {
+world.add(world.r)
+worl
+
+}
